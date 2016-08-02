74107137c946115b28d105a3ba2edf557911a066
Stanley Russian <stan.russian@gmail.com>
2016-08-03 00:10:15 +0300

    Added explorer tab for every entity

---
+++ ModernUI/Pages/Explore/Packages/IndividualPackage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace UI.Pages.Explore.Packages
+{
+    /// <summary>
+    /// Interaction logic for IndividualPackage.xaml
+    /// </summary>
+    public partial class IndividualPackage : UserControl
+    {
+        public IndividualPackage()
+        {
+            InitializeComponent();
+        }
+    }
+}
