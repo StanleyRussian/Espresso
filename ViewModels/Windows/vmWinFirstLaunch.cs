7cedd86c44d60b743e6f386605d602f2e5ea34ef
Tupc_ho91 <stan.russian@gmail.com>
2016-08-28 13:04:19 +0300

    Fixed bugs in first launch window, and windows now accept commas

--- ViewModels/Windows/vmWinFirstLaunch.cs
+++ ViewModels/Windows/vmWinFirstLaunch.cs
@@ -5,6 +5,8 @@ using MahApps.Metro.Controls.Dialogs;
 using Model;
 using Model.Entity;
 using ViewModels.Auxiliary;
+using System;
+using System.Windows;
 
 namespace ViewModels.Windows
 {
@@ -13,6 +15,7 @@ namespace ViewModels.Windows
         public vmWinFirstLaunch()
         {
             cmdGo = new Command(cmdGo_Execute);
+            AccountName = "Наличка";
             CoffeeSorts = new ObservableCollection<hCoffeeSort>();
         }
 
@@ -21,40 +24,74 @@ namespace ViewModels.Windows
         public ObservableCollection<hCoffeeSort> CoffeeSorts { get; set; }
         public ICommand cmdGo { get; set; }
 
-        private void cmdGo_Execute()
+        private void cmdGo_Execute(object argWindow)
         {
-            if (AccountName == "" || AccountBalance < 0) 
+            try
             {
-                DialogCoordinator.Instance.ShowMessageAsync(this, "Ошибка", "Заполните все поля",
-                    MessageDialogStyle.Affirmative,
-                    new MetroDialogSettings {ColorScheme = MetroDialogColorScheme.Accented});
-                return;
-            }
-
-            ContextManager.Context.Accounts.Add(new Account {Name = AccountName});
-            ContextManager.Context.dAccountsBalances.First(p => p.Account.Name == AccountName).Balance = AccountBalance;
-            foreach (var coffeeSort in CoffeeSorts)
-            {
-                if (coffeeSort.Name == "" || 
-                    coffeeSort.Cost < 0 || 
-                    coffeeSort.GreenStocks < 0 || 
-                    coffeeSort.RoastedStocks < 0) 
+                if (AccountName == "" || AccountName == null || AccountBalance < 0)
                 {
-                    DialogCoordinator.Instance.ShowMessageAsync(this, "Ошибка", "Заполните все поля таблицы",
+                    DialogCoordinator.Instance.ShowMessageAsync(this, "Ошибка", "Заполните все поля",
                         MessageDialogStyle.Affirmative,
                         new MetroDialogSettings { ColorScheme = MetroDialogColorScheme.Accented });
                     return;
                 }
-                ContextManager.Context.CoffeeSorts.Add(new CoffeeSort {Name = coffeeSort.Name});
-                var dGreenStock = ContextManager.Context.dGreenStocks.First(p => p.CoffeeSort.Name == coffeeSort.Name);
-                dGreenStock.Quantity = coffeeSort.GreenStocks;
-                dGreenStock.dCost = coffeeSort.Cost;
 
-                var dRoastedStock = ContextManager.Context.dRoastedStocks.First(p => p.CoffeeSort.Name == coffeeSort.Name);
-                dRoastedStock.Quantity = coffeeSort.RoastedStocks;
-                dRoastedStock.dCost = coffeeSort.Cost;
+                ContextManager.Context.Accounts.Add(new Account { Name = AccountName });
+                ContextManager.Context.SaveChanges();
+
+                ContextManager.Context.dAccountsBalances.First(p => p.Account.Name == AccountName).Balance = AccountBalance;
+                ContextManager.Context.SaveChanges();
+
+                if (CoffeeSorts.Count == 0
+                    || CoffeeSorts.Count == 1
+                        && CoffeeSorts[0].Name == ""
+                        && CoffeeSorts[0].Name == null
+                        && CoffeeSorts[0].Cost == 0
+                        && CoffeeSorts[0].GreenStocks == 0
+                        && CoffeeSorts[0].RoastedStocks == 0)
+                {
+                    Properties.FirstLaunch = false;
+                    var window1 = argWindow as Window;
+                    window1.Close();
+                    return;
+                }
+
+                foreach (var coffeeSort in CoffeeSorts)
+                {
+                    if (coffeeSort.Name == ""
+                        || coffeeSort.Name == null
+                        || coffeeSort.Cost < 0
+                        || coffeeSort.GreenStocks < 0
+                        || coffeeSort.RoastedStocks < 0)
+                    {
+                        DialogCoordinator.Instance.ShowMessageAsync(this, "Ошибка", "Заполните все поля таблицы",
+                            MessageDialogStyle.Affirmative,
+                            new MetroDialogSettings { ColorScheme = MetroDialogColorScheme.Accented });
+                        return;
+                    }
+                    ContextManager.Context.CoffeeSorts.Add(new CoffeeSort { Name = coffeeSort.Name });
+                    ContextManager.Context.SaveChanges();
+
+                    var dGreenStock = ContextManager.Context.dGreenStocks.First(p => p.CoffeeSort.Name == coffeeSort.Name);
+                    dGreenStock.Quantity = coffeeSort.GreenStocks;
+                    dGreenStock.dCost = coffeeSort.Cost;
+
+                    var dRoastedStock = ContextManager.Context.dRoastedStocks.First(p => p.CoffeeSort.Name == coffeeSort.Name);
+                    dRoastedStock.Quantity = coffeeSort.RoastedStocks;
+                    dRoastedStock.dCost = coffeeSort.Cost;
+
+                    ContextManager.Context.SaveChanges();
+                }
 
                 Properties.FirstLaunch = false;
+                var window = argWindow as Window;
+                window.Close();
+            }
+            catch (Exception ex)
+            {
+                DialogCoordinator.Instance.ShowMessageAsync(this, "Ошибка", ex.Message,
+                    MessageDialogStyle.Affirmative,
+                    new MetroDialogSettings { ColorScheme = MetroDialogColorScheme.Accented });
             }
         }
     }
